62ba9d9c2259a231e5abf9442d66b8932e1277e8
chuongdkph26546 <chuongdkph26546@fpt.edu.vn>
2022-11-14 15:32:05 +0700

    update layout

---
+++ app/src/main/java/com/example/duan_n6_cp17303/Fragment/FragmentThem.java
@@ -0,0 +1,38 @@
+package com.example.duan_n6_cp17303.Fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.duan_n6_cp17303.R;
+
+
+public class FragmentThem extends Fragment {
+
+
+    public static FragmentThem newInstance() {
+        FragmentThem fragment = new FragmentThem();
+
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_them, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
