210e162ae4b21a9afd4b30c76750baeea3c9d2ce
sangpxph26484 <sangpxph26484@fpt.edu.vn>
2022-11-24 23:59:11 +0700

    Update layout sangdzai

--- app/src/main/java/com/example/duan_n6_cp17303/Manhinhchao.java
+++ app/src/main/java/com/example/duan_n6_cp17303/Manhinhchao.java
@@ -15,11 +15,11 @@ public class Manhinhchao extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Manhinhchao.this,MainActivity.class);
+                Intent intent = new Intent(Manhinhchao.this,DangNhapActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },5000);
+        },3000);
     }
 
 }
